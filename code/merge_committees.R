bc9f206b70cf7b7ec94e0aaf76b54944267371b9
judgelord <devin.jl@gmail.com>
2026-06-29 12:13:32 -0400

    changes "oversight" to "reporting_agencies" to avoid confusion with a duplicated variable name -- could have breaking changes with other repo code, but everything here is fixed

--- code/merge_committees.R
+++ code/merge_committees.R
@@ -6,20 +6,34 @@ here::here("data", "members_committees_oversight.rda") |>
 
 
 members_committees_oversight %<>%
-  # remove vars with NAs from merge of historical committee data in https://github.com/judgelord/committees/issues/1
-  select(icpsr, congress, committees, oversight, chair, ranking_minority, titles, positions)
+  #FIXME in committees repo - this is a bad name
+  rename(reporting_agencies = oversight) %>%
+   # remove vars with NAs from merge of historical committee data in https://github.com/judgelord/committees/issues/1
+  distinct(icpsr, congress, committees, reporting_agencies, chair, ranking_minority, titles, positions)
 
 
 member_data <- member_data |>
   left_join(members_committees_oversight)
 
+#TODO fix missing data  https://github.com/judgelord/committees/issues/4
 
 if(FALSE){ # TESTING / LOOOKING FOR ERRORS
 member_data |>
   count(is.na(committees))
 
   member_data |>
-    count(is.na(oversight))
+    filter(is.na(committees),
+           !state %in% c("district of columbia", "puerto rico")) |>
+    drop_na(state) |>
+    count(icpsr, bioname, congress, chamber,
+            state, district_code,
+          committees,
+              # chair,  ranking_minority,
+             sort = T) |> arrange(district_code, bioname) |> kable()
+          #congress)
+
+  member_data |>
+    count(is.na(reporting_agencies))
 
 member_data |> count(positions, chair, ranking_minority)
 
@@ -38,3 +52,5 @@ look |>
   distinct(icpsr, bioname, congress, committees, positions) |>
   write_csv(file = here::here("data", "to correct by hand", "committee_corrections.csv"))
 }
+
+
