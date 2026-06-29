8e9e35ac8d64c7c5d99da9521b70d73779759b80
judgelord <devin.jl@gmail.com>
2026-06-29 12:04:07 -0400

    improved make workflow

--- _make_book.R
+++ _make_book.R
@@ -5,16 +5,18 @@ if(F){
   here::here("code", "make_member_data.R") |> source()
   # make total and type 1-5 datasets
   here::here("code", "replication-data.qmd") |> quarto::quarto_render(cache_refresh = TRUE)
-  # run one replication file
-  here::here("replication-total.qmd") |> quarto::quarto_render(cache_refresh = TRUE)
-  here::here("replication-1.qmd") |> quarto::quarto_render(cache_refresh = TRUE)
-  here::here("replication-2.qmd") |> quarto::quarto_render(cache_refresh = TRUE)
-  here::here("replication-3.qmd") |> quarto::quarto_render(cache_refresh = TRUE)
-  here::here("replication-4.qmd") |> quarto::quarto_render(cache_refresh = TRUE)
-  here::here("replication-5.qmd") |> quarto::quarto_render(cache_refresh = TRUE)
+  # run one replication file at a time
+  refresh = F # refresh if data or models used in manuscript tables changed, don't if just adding new things
+  job = F
+  here::here("replication-total.qmd") |> quarto::quarto_render(cache_refresh = refresh, as_job = job)
+  here::here("replication-1.qmd") |> quarto::quarto_render(cache_refresh = refresh, as_job = job)
+  here::here("replication-2.qmd") |> quarto::quarto_render(cache_refresh = refresh, as_job = job)
+  here::here("replication-3.qmd") |> quarto::quarto_render(cache_refresh = refresh, as_job = job)
+  here::here("replication-4.qmd") |> quarto::quarto_render(cache_refresh = refresh, as_job = job)
+  here::here("replication-5.qmd") |> quarto::quarto_render(cache_refresh = refresh, as_job = job)
 
   # run all 6 replication files
-  quarto::quarto_render(input = here::here(), profile = "replication", cache_refresh = TRUE)
+  quarto::quarto_render(input = here::here(), profile = "replication", cache_refresh = refresh)
 }
 
 # if you want, preview the book in HTML (the pdf is in the /docs/ folder)
@@ -23,7 +25,7 @@ quarto::quarto_preview()
 # to stop the preview (required to run other code)
 quarto::quarto_preview_stop()
 
-quarto::quarto_render()
+quarto::quarto_render(as_job = F)
 
 # send to git (just Devin for now)
 if(F){
