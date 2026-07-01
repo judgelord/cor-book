f18205596ad8a889c953a87f8e49f7376c79bca4
judgelord <devin.jl@gmail.com>
2026-07-01 11:05:37 -0400

    arrange vars in tables to match framework

--- code/cm.R
+++ code/cm.R
@@ -2,43 +2,63 @@ library(stringr)
 
 # Coef Map
 cm = c(
-  # $
-  "PACamount_m" = "Corporate Campaign $ (Millions)",
-  "PACamount_energy_m" = "Energy Corp. Campaign $ (Millions)",
-  "PACamount_nonenergy_m" = "Non-energy Corp. Campaign $ (Millions)",
+  # district-level
+  "new_memberTRUE" = "New Member",
+  "new_member" = "New Member",
+  "new_senator" = " New Senator in Delegation",
+  "new_one" = " New Member in Delegation",
+  "new_proportion" = " New Proportion in Delegation",
+  "new_member:same_party" = " New Member x Same Party",
+  "new_memberTRUE:same_party" = "New Member x Same Party",
+  "same_party:second" = "Second Year x Same Party",
+  "same_party:third" = "Third Year x Same Party",
+  "same_party:fourth" = "Fourth Year x Same Party",
+  "same_party:fifth" = "Fifth Year x Same Party",
+  "same_party:sixth" = "Sixth Year x Same Party",
+  "same_party" = "Same Party",
+  #############################################################
+  # DEMAND
+  ###############################################################
   # census (old)
   "pop2010m"= "State Population (Millions)",
   # census (new)
-       "population_total" = "Population",
-       "population_m" = "Population (Millions)",
-  "workers_federal" = "Federal employed (Millions)" ,
+  "population_total" = "Population",
+  "population_m" = "Population (Millions)",
+  "workers_federal" = "Federal Employed (Millions)" ,
   "workers_total" = "workers_total (Millions)" ,
-  "workers_private_sector" = "Private sector (Millions)" ,
-  "workers_self_employed" = "Self employed (Millions)" ,
-  "workers_nonprofit" = "Nonprofit employed (Millions)" ,
-  "workers_government" = "Government employed (Millions)" ,
+  "workers_private_sector" = "Private Sector (Millions)" ,
+  "workers_self_employed" = "Self Employed (Millions)" ,
+  "workers_nonprofit" = "Nonprofit Employed (Millions)" ,
+  "workers_government" = "Government Employed (Millions)" ,
   # "workers_nonprofit_2" = "workers_nonprofit_2 (Millions)" ,
   "education_bachelor_or_higher" = "College Grads (Millions)" ,
-  "public_assistance_or_snap_households" = "Receiving public assistance (Millions)" ,
+  "public_assistance_or_snap_households" = "Receiving Public Assistance (Millions)" ,
   # Percents from census
   "percent_workers_federal" = "% Federal Employees",  #FIXME this one is from a different section, not sure what to use for the denominator
-       # workers section of census
+  # workers section of census
   "percent_workers_nonprofit" = "% Nonprofit Sector" ,
   "percent_workers_self_employed"  = "% Self Employed",
   "percent_workers_private_sector"  = "% Private Sector",
   "percent_workers_government"  = "% Government Sector",
-       # other sections
+  # other sections
   "percent_education_bachelor_or_higher" = "% College Grad",
   "percent_public_assistance_or_snap_households" = "% Public Assistance",
+  ##################################################################################
+  # Strategic responses to political environment
+  #################################################################################
   # STYLES FROM CROSSON AND KAVLOFSKI
   "styleDistrict Advocate" = "District Advocate",
   "styleParty Builder" = "Party Builder",
   "styleParty Soldier" = "Party Soldier",
   "stylePolicy Specialist" = "Policy Specialist",
+  # $
+  "PACamount_m" = "Corporate Campaign $ (Millions)",
+  "PACamount_energy_m" = "Energy Corp. Campaign $ (Millions)",
+  "PACamount_nonenergy_m" = "Non-energy Corp. Campaign $ (Millions)",
   # ELECTORAL
-       "competitive" = "Competitive General",
-       "competitive_primary" = "Competitive Primary",
-  # staff
+  "competitive" = "Competitive General",
+  "competitive_primary" = "Competitive Primary",
+  # staff allocation
   "pct_MRA_spending" = "% MRA Spent",
   "pct_MRA_legis_spending"   = "% MRA Legislative",
   "pct_MRA_pol_spending"   = "% MRA Political",
@@ -69,69 +89,60 @@ cm = c(
   "est_off_spending" = "Office Spending",
   "est_constit_spending" = "Const. Spending",
   # VOTEVIEW
-"majority" = "Majority",
-"presidents_party" = "President's party",
-       "abs_nominate_dim1" = "abs(NOMINATE)",
-  "abs(nominate_dim1)" = "abs(NOMINATE)",
-       "abs(agency_ideo)" = "Agency Percieved as Ideological",
-       "abs_nominate_dim1:presidents_party" = "abs(NOMINATE) x President's party",
+  "majority" = "Majority",
+  "presidents_party" = "President's Party",
   "party(R)" = "Republican",
   "party(I)" = "Independent",
-       # RCL + VOTEVIEW
-       "alignmentTRUE" = "Aligned with Agency",
-       "distance" = "Ideological Distance",
-       "distance:presidents_party" = "Ideological Distance x President's party",
-       "presidents_party:distance" = "Ideological Distance x President's party",
-       # EXPERIENCE
-       "new_memberTRUE" = "New Member",
-       "new_member" = "New Member",
-       "new_senator" = " New Senator in Delegation",
-       "new_one" = " New Member in Delegation",
-       "new_proportion" = " New Proportion in Delegation",
-       "new_member:same_party" = " New Member x Same Party",
-       "first_two" = "First Two Years",
-       "experience" = "Three+ Years in Office",
-       "first" = "First Year",
-       "second" = "Second Year",
-       "third" = "Third Year",
-       "fourth" = "Fourth Year",
-       "fifth" = "Fifth Year",
-       "sixth" = "Sixth Year",
-       "new_memberTRUE:same_party" = "New Member x Same Party",
-       "same_party:second" = "Second Year x Same Party",
-       "same_party:third" = "Third Year x Same Party",
-       "same_party:fourth" = "Fourth Year x Same Party",
-       "same_party:fifth" = "Fifth Year x Same Party",
-       "same_party:sixth" = "Sixth Year x Same Party",
-       "same_party" = "Same Party",
+  # RCL + VOTEVIEW
+  "alignmentTRUE" = "Aligned with Agency",
+  "distance" = "Ideological Distance",
+  "distance:presidents_party" = "Distance x President's Party",
+  "presidents_party:distance" = "Distance x President's Party",
+  "abs(agency_ideo)" = "Agency Percieved as Ideological",
+  "abs_nominate_dim1:presidents_party" = "abs(NOMINATE) x President's Party",
+  "abs_nominate_dim1" = "|DW-NOMINATE-1|",
+  "abs(nominate_dim1)" = "|DW-NOMINATE-1|",
+  #############################
+  # Capacity
+  #################################################
   # institutional position
-"oversight" = "Oversight Committee",
+  "oversight" = "Reporting Committee",
+  # EXPERIENCE
+  "first_two" = "First Two Years",
+  "experience" = "Three+ Years in Office",
+  "first" = "First Year",
+  "second" = "Second Year",
+  "third" = "Third Year",
+  "fourth" = "Fourth Year",
+  "fifth" = "Fifth Year",
+  "sixth" = "Sixth Year",
   "chair" = "Committee Chair",
   "ranking_minority" = "Ranking Member",
   "prestigeR2" = "Prestige Committee (R2)",
   "prestigeOLD" = "Prestige Committee (Old)",
   "prestige" = "Prestige Committee",
-# CEL (LES)
-"state_leg" = "Served in State Leg.",
-"subchr" = "Subcommittee Chiar",
-"lesclassic" = "LES (classic)",
-"seniority" = "Seniority",
-"freshman" = "Freshman",
-"female"  = "Female",
-"afam"= "African American" ,
-"latino" = "Latino",
-"votepct" = "Vote Percent",
-#FE
-       "Legislator" = "Legislator",
-       "Agency" = "Agency",
-       "Num.Obs." = "Observations"
+  # CEL (LES)
+  "subchr" = "Subcommittee Chair",
+  "state_leg" = "Served in State Leg.",
+  "lesclassic" = "LES (classic)",
+  "seniority" = "Seniority",
+  "freshman" = "Freshman",
+  "female"  = "Female",
+  "afam"= "African American" ,
+  "latino" = "Latino",
+  "votepct" = "Vote Percent",
+  "chamberSenate" = "Senate",
+  #FE
+  "Legislator" = "Legislator",
+  "Agency" = "Agency",
+  "Num.Obs." = "Observations"
 )
 
-# FORMATTING FOR AJPS
-cmAJPS <- cm |> str_to_sentence()
-names(cmAJPS) <- names(cm)
-cm <- cmAJPS
-# END FORMATTING FOR AJPS
+# # FORMATTING FOR AJPS
+# cmAJPS <- cm |> str_to_sentence()
+# names(cmAJPS) <- names(cm)
+# cm <- cmAJPS
+# # END FORMATTING FOR AJPS
 
 # set fixed effects mapping
 
